f5cc65b3279594fa71b31a8d0fbe2405b3dee6b7
Igor Volokho <igorvolokho99@gmail.com>
2025-01-18 14:03:18 +0300

    # 38 New button was added.

--- client_service/helpers/button_class.py
+++ client_service/helpers/button_class.py
@@ -2,36 +2,44 @@ import pygame
 
 pygame.init()
 
-font = pygame.font.Font(None, 20)
+font = pygame.font.Font(None, 26)
 
 
 class Button:
-    def __init__(self, text, x, y, w, h, color_active=(50, 50, 50), color_inactive=(255, 0, 0), font=font):
+    def __init__(self, text, x, y, w, h, color_base=(65, 105, 225), color_hover=(100, 149, 237), shadow_color=(20, 20, 20), font=font):
         self.rect = pygame.Rect(x, y, w, h)
         self.text = text
         self.font = font
-        self.txt_surface = self.font.render(self.text, True, (255, 255, 255))
-        self.color_active = color_active
-        self.color_inactive = color_inactive
-        self.color = self.color_inactive
-        self.x, self.y, self.w, self.h = x, y, w, h
+        self.color_base = color_base  # Основной цвет кнопки
+        self.color_hover = color_hover  # Цвет при наведении
+        self.shadow_color = shadow_color  # Цвет тени
+        self.current_color = color_base
 
     def handle_event(self, event):
+        """Обрабатывает события кнопки."""
         if event.type == pygame.MOUSEBUTTONUP:
             if self.rect.collidepoint(pygame.mouse.get_pos()):
                 return True
 
     def draw(self, screen):
+        """Рисует кнопку с прямоугольной тенью."""
         mouse = pygame.mouse.get_pos()
-        pygame.draw.rect(screen, (50, 150, 200), (self.x, self.y, self.w, self.h))
+        is_hovered = self.rect.collidepoint(mouse)
 
-        # if self.x + self.w > mouse[0] > self.x and self.y + self.h > mouse[1] > self.y:
-        #     pygame.draw.rect(screen, (50, 200, 255), (self.x, self.y, self.w, self.h))
-        if self.rect.collidepoint(mouse):
-            self.color = self.color_active
-            pygame.draw.rect(screen, (50, 200, 255), (self.x, self.y, self.w, self.h))
-        else:
-            self.color = self.color_inactive
-        text_surface = self.font.render(self.text, True, self.color)
-        text_rect = text_surface.get_rect(center=((self.rect.x + self.rect.w / 2), (self.rect.y + self.rect.h / 2)))
+        # Параметры тени
+        shadow_offset = 8 if not is_hovered else 4  # Тень уменьшается при наведении
+        shadow_rect = pygame.Rect(self.rect.x + shadow_offset, self.rect.y + shadow_offset, self.rect.width,
+                                  self.rect.height)
+
+        # Рисуем тень
+        pygame.draw.rect(screen, self.shadow_color, shadow_rect)
+
+        # Цвет кнопки
+        self.current_color = self.color_hover if is_hovered else self.color_base
+        pygame.draw.rect(screen, self.current_color, self.rect)
+
+        # Текст
+        text_surface = self.font.render(self.text, True, (255, 255, 255))  # Белый текст
+        text_rect = text_surface.get_rect(
+            center=(self.rect.x + self.rect.width / 2, self.rect.y + self.rect.height / 2))
         screen.blit(text_surface, text_rect)
