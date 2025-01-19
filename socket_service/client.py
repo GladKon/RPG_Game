37d5ee95a818bd57fa7742baf39069b3e63031ad
Artem Modin <artemm0din@yandex.ru>
2025-01-19 15:53:32 +0300

    3-commit. add in client main and handle_message_to_server

--- socket_service/client.py
+++ socket_service/client.py
@@ -1,43 +1,67 @@
 import socket
-import json
+import sys
+import threading
 import time
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('127.0.0.1', 8080))
 
-end = time.time()
-while True:
-    start = time.time()
-    if start - end >= 5:
-        data = b'Hello, i am bot'
-        client_socket.send(data)
-        end = time.time()
 
+def handle_message_to_server(client_socket):
+    while True:
         data = client_socket.recv(1024)
         print(f'Server data: {data.decode()}')
 
 
-client_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket1.connect(('127.0.0.2', 8080))
+def main():
+    server_ip = '127.0.0.1'
+    server_port = 8080
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_ip, server_port))
+    thread_socket = threading.Thread(target=handle_message_to_server, args=(client_socket, ))
+
+    try:
+
+        thread_socket.start()
+        while True:
+            print('Введите сообщение.')
+            data = input()
+            client_socket.send(data.encode('utf-8'))
 
 
-while True:
-    start = time.time()
-    if start - end >= 5:
-        data = b'Hello, i am player'
-        client_socket1.send(data)
-        end = time.time()
 
-        data = client_socket1.recv(1024)
-        print(f'Server data: {data.decode()}')
+    except ConnectionRefusedError:
+        print(f"Не удалось подключится к серверу {server_ip}:{server_port}.")
+    except Exception as e:
+        print(f'Произошла ошибка {e}')
+    finally:
+        client_socket.close()
+        sys.exit()
+if __name__ == "__main__":
+    main()
 
-client_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket2.connect(('127.0.0.2', 12345))
-data = [1234,56453,23521356]
 
-client_socket2.send(json.dumps(data).encode('UTF-8'))
-
-data = client_socket2.recv(1024)
-print(f'Server data: {data.decode()}')
+#
+#
+# client_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client_socket1.connect(('127.0.0.2', 8080))
+#
+#
+# while True:
+#     start = time.time()
+#     if start - end >= 5:
+#         data = b'Hello, i am player'
+#         client_socket1.send(data)
+#         end = time.time()
+#
+#         data = client_socket1.recv(1024)
+#         print(f'Server data: {data.decode()}')
+#
+# client_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client_socket2.connect(('127.0.0.2', 12345))
+# data = [1234,56453,23521356]
+#
+# client_socket2.send(json.dumps(data).encode('UTF-8'))
+#
+# data = client_socket2.recv(1024)
+# print(f'Server data: {data.decode()}')
 
 # client_socket.close()
 # client_socket1.close()
